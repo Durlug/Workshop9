5f05f4f129fe06901b0a3265b690f769aed10480
Durlug <shaun.g.richardson@gmail.com>
2016-11-08 11:36:54 -0500

    Add files via upload

---
+++ Hero.h
@@ -0,0 +1,32 @@
+//Workshop 8
+//Hero.h
+//Shaun Richardson
+//110392156
+//srichardson6@myseneca.ca
+
+#ifndef HERO_H
+#define HERO_H
+
+#include <iostream>
+
+namespace ict
+{
+	class Hero
+	{
+	private:
+		char _name[21];
+		double _strength;
+	public:
+		Hero();
+		Hero(const char*, double);
+		bool isEmpty() const;
+		double getStrength() const;
+		void display(std::ostream&) const;
+		void operator-=(double strength);
+		void operator+=(double strength);
+		friend bool operator<(const Hero&, const Hero&);
+	};
+
+}
+
+#endif
